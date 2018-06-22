9646eba1ae35e229b150aaea0c995b80f3d00fb0
liuhuihuang <liuhh@wise2c.com>
2018-06-22 12:08:54 +0800

    xiugai

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://0.0.0.0:80/api/v1"',
+  BASE_API: '"http://localhost:80/api/v1"',
 })
