e4b4c223d30e17648efa452001c8b9a517a57ae6
wen.xing <wenxing@wise2c.com>
2018-09-04 15:36:58 +0800

    #572

--- src/views/contact_record_dail/index.vue
+++ src/views/contact_record_dail/index.vue
@@ -167,38 +167,39 @@
   <div class='container' v-else>
     <div class="el-icon-info">接触记录详情</div>
     <el-row :gutter="0">
-        <el-form  :model="detailInfo" ref="contactDetailForm" label-width="100px">
-          <el-col :span="4">
+        <el-form  :model="detailInfo" ref="contactDetailForm" label-width="90px">
+          <el-col :span="5">
             <div class="grid-content bg-purple">
-              <el-form-item label="接触编号：" prop="contactInfo.recordId">
+              <el-form-item label="接触编号:" prop="contactInfo.recordId">
                 <span>{{detailInfo.contactInfo.recordId}}</span>
               </el-form-item>
-              <el-form-item  label="通话时长：" prop="contactInfo.talkTime">
+              <el-form-item  label="通话时长:" prop="contactInfo.talkTime">
                 <span>{{formatSeconds(detailInfo.contactInfo.talkTime)}}</span>
               </el-form-item>
-              <el-form-item  label="小结备注：" prop="contactInfo.description" size="small">
-                <el-input type="textarea" v-model="detailInfo.contactInfo.description"></el-input>
+              <el-form-item  label="小结备注:" prop="contactInfo.description" size="small">
+                <div style='width:80px;white-space:nowrap;text-overflow:ellipsis;overflow:hidden;' :title="detailInfo.contactInfo.description?detailInfo.contactInfo.description:'无'">{{detailInfo.contactInfo.description?detailInfo.contactInfo.description:"无"}}</div>
               </el-form-item>
             </div>
           </el-col>
           <el-col :span="4">
             <div class="grid-content bg-purple">
-              <el-form-item  label="员工姓名：">
+              <el-form-item  label="员工姓名:">
                 <span>{{detailInfo.staffInfo.staffName}}</span>
               </el-form-item>
-               <el-form-item  label="员工工号：" prop="contactInfo.staffId">
+               <el-form-item  label="员工工号:" prop="contactInfo.staffId">
                 <span>{{detailInfo.contactInfo.staffId}}</span>
               </el-form-item>
 
-                <el-form-item  label="话后小结：">
-                  <el-tree
+                <el-form-item  label="话后小结:">
+                   <div  style='width:80px;white-space:nowrap;text-overflow:ellipsis;overflow:hidden;' :title="showSummaryInfo(detailInfo.contactInfo.summaryDetailInfos)">{{showSummaryInfo(detailInfo.contactInfo.summaryDetailInfos)}}</div>
+                  <!-- <el-tree
                     ref = "tree"
                     :data="detailInfo.summariesInfo"
                     show-checkbox
                     node-key="id"
                     default-expand-all
                     :props="defaultProps">
-                  </el-tree>
+                  </el-tree> -->
                </el-form-item>
             </div>
           </el-col>
@@ -213,58 +214,50 @@
             </div>
           </el-col> -->
           <el-col :span="4">
-             <el-form-item  label="主叫：" prop="contactInfo.callerNumber">
+             <el-form-item  label="主叫号码:" prop="contactInfo.callerNumber">
                 <span>{{detailInfo.contactInfo.callerNumber}}</span>
               </el-form-item>
-              <el-form-item   label="被叫：" prop="contactInfo.calleeNumber">
+              <el-form-item   label="被叫号码:" prop="contactInfo.calleeNumber">
                   <span>{{hideMobile(detailInfo.contactInfo.calleeNumber)}}</span>
               </el-form-item>
-              <el-form-item  label="客户姓名：" prop="contactInfo.customerName" size="small">
+              <el-form-item  label="客户姓名:" prop="contactInfo.customerName" size="small">
                 <span>{{detailInfo.contactInfo.customerName}}</span>
               </el-form-item>
           </el-col>
           <el-col :span="5">
             <div class="grid-content bg-purple">
-              <el-form-item label="拨打时间：" prop="contactInfo.callTime">
+              <el-form-item label="拨打时间:" prop="contactInfo.callTime">
                 <span>{{detailInfo.contactInfo.callTime}}</span>
               </el-form-item>
-              <el-form-item  label="接听时间：" prop="contactInfo.answerTime">
+              <el-form-item  label="接听时间:" prop="contactInfo.answerTime">
                 <span>{{(detailInfo.contactInfo.answerTime)== null?'无':(detailInfo.contactInfo.answerTime)}}</span>
               </el-form-item>
-               <el-form-item  label="身份证：" prop="customerInfo.idNumber">
+               <el-form-item  label="身份证号:" prop="customerInfo.idNumber">
                 <span>{{hideIdNumber(detailInfo.customerInfo.idNumber)}}</span>
               </el-form-item>
             </div>
           </el-col>
           <el-col :span="6">
             <div class="grid-content bg-purple">
-              <el-form-item  label="电话录音：" prop="contactInfo.soundRecordUrl">
-                <audio v-bind:src="detailInfo.contactInfo.soundRecordUrl" controls="controls"></audio>
-              </el-form-item>
-               <el-form-item  label="任务状态：" prop="dialTaskInfo.status" size="small">
-                <el-radio-group v-model="detailInfo.dialTaskInfo.status">
-                  <el-radio  label="2">成功</el-radio>
-                  <el-radio  label="3">失败</el-radio>
-                  <el-radio  label="1">预约</el-radio>
+               <el-form-item  label="任务状态:">
+                  <span v-if="detailInfo.dialTaskInfo.status=='2'">成功</span>
+                  <span v-if="detailInfo.dialTaskInfo.status=='3'">失败</span>
+                  <span v-if="detailInfo.dialTaskInfo.status=='1'">预约</span>
                </el-radio-group>
               </el-form-item>
-              <el-form-item  label="预约时间：" prop="dialTaskInfo.appointTime"  v-if="detailInfo.dialTaskInfo.status=='1'"  size="small">
-                <el-date-picker
-                  v-model="detailInfo.dialTaskInfo.appointTime"
-                  type="datetime"
-                  value-format="yyyy-MM-dd HH:mm:ss"
-                  default-time="00:00:00"
-                  placeholder="选择日期时间"  size="small">
-                 </el-date-picker>
+              <el-form-item  label="预约时间:" prop="dialTaskInfo.appointTime"  v-if="detailInfo.dialTaskInfo.status=='1'" >
+                <span>{{detailInfo.dialTaskInfo.appointTime?detailInfo.dialTaskInfo.appointTime:'无'}}</span>
+              </el-form-item>
+              <el-form-item  label="电话录音:" prop="contactInfo.soundRecordUrl">
+                <audio v-bind:src="detailInfo.contactInfo.soundRecordUrl" controls="controls" style="height:40"></audio>
               </el-form-item>
             </div>
           </el-col>
       </el-form>
     </el-row>
     <div slot="footer" style="text-align: right;">
-          <el-button type="danger" @click="resetDetai();contactDetail()">重 置</el-button>
           <el-button @click="isMainPage = true">取 消</el-button>
-          <el-button type="primary" @click="checkEdit()">修改</el-button>
+          <el-button type="primary" @click="editVisible=true">修改</el-button>
     </div>
   <div class="el-icon-info">相关接触记录列表</div>
   <el-row>
@@ -356,7 +349,7 @@
         </el-table>
       </el-col>
     </el-row>
-    <!-- 订单详情 -->
+      <!-- 订单详情 -->
       <el-dialog
       align:left
       width="40%"
@@ -396,10 +389,63 @@
           <el-button @click="detailVisible = false">返回</el-button>
         </div>
       </el-dialog>
-</div>
+      <!-- 接触记录修改 -->
+      <el-dialog
+      align:left
+      width="30%"
+      title="接触历史修改"
+      :visible.sync="editVisible"
+      append-to-body>
+      <el-form  :model="detailInfo" ref="contactDetailForm" label-width="100px">
+        <el-form-item label="任务状态：" prop="dialTaskInfo.status">
+          <el-radio-group v-model="detailInfo.dialTaskInfo.status">
+              <el-radio  label="2">成功</el-radio>
+              <el-radio  label="3">失败</el-radio>
+              <el-radio  label="1">预约</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="预约时间：" prop="dialTaskInfo.appointTime" v-if="detailInfo.dialTaskInfo.status=='1'"  size="small">
+          <el-date-picker
+            v-model="detailInfo.dialTaskInfo.appointTime"
+            type="datetime"
+            value-format="yyyy-MM-dd HH:mm:ss"
+            default-time="00:00:00"
+            placeholder="选择日期时间"  size="small">
+         </el-date-picker>
+        </el-form-item>
+        <el-form-item label="话后小结：">
+          <el-tree
+            ref = "tree"
+            :data="detailInfo.summariesInfo"
+            show-checkbox
+            node-key="id"
+            :default-checked-keys="keys"
+            default-expand-all
+            :props="defaultProps">
+          </el-tree>
+          </el-form-item>
+        <el-form-item label="小结备注：" prop="contactInfo.description">
+          <el-input type="textarea" v-model="detailInfo.contactInfo.description"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" style="text-align: right;">
+        <el-button type="danger" @click="resetDetai();contactDetail()">重 置</el-button>
+        <el-button @click="editVisible = false;resetDetai();contactDetail()">取 消</el-button>
+        <el-button type="primary" @click="checkEdit()">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <style lang='scss' scoped>
+audio {
+    width: 250px;
+    height: 45px;
+}
+.span-class{
+  width: 80px;
+  height: 40px;
+}
 </style>
 
 <script>
@@ -426,6 +472,8 @@
 
     data() {
       return {
+        keys: [],
+        editVisible: false, // 修改
         showTalkTime: true,
         detailVisible: false, // 订单详情展示开关
         isMainPage: true, // 显示或隐藏详情页面
@@ -598,12 +646,13 @@
       },
       // 显示小结信息
       showSummarys(nodules) {
-        if (nodules !== null && nodules.length > 0) {
-          var names = ''
+        if (nodules && nodules.length > 0) {
+          var names = []
           for (var i = 0; i < nodules.length; i++) {
-            names += nodules[i].name + '/'
+            names.push(nodules[i].name)
           }
-          names = names.substr(0, (names.length - 1))
+          console.log('names:', names)
+          names = names.join('，')
           return names
         } else {
           return '无'
@@ -627,7 +676,9 @@
                 return
               }
             })
-            this.$refs.tree.setCheckedKeys(a)
+            this.keys = []
+            this.keys = a
+            // this.$refs.tree.setCheckedKeys(a)
           }
         })
         queryrecordbytaskid(this.ids.taskId, this.ids.campaignId).then(response => {
@@ -698,7 +749,9 @@
               generateRecord(recordId, nodules, description).then(response => {
                 if (response.data.code === 0) {
                   this.searchByKeyWords(this.req)
-                  this.isMainPage = true
+                  this.editVisible = false
+                  this.resetDetai()// 刷新
+                  this.contactDetail()// 刷新
                   this.$message.success('修改成功')
                 } else {
                   this.$message.error(response.data.message)
@@ -806,18 +859,15 @@
       },
       // 详情小结展示
       showSummaryInfo(summary) {
-        var html = ''
-        if (summary.length === 0) {
-          html = '无'
+        var html = []
+        if (!summary || summary.length === 0) {
+          html.push('无')
         } else {
           for (var j in summary) {
-            html += summary[j].name + ', '
-          }
-          if (html.indexOf(', ') > -1) {
-            html = html.substring(0, html.length - 1)
+            html.push(summary[j].name)
           }
         }
-        return html
+        return html.join('，')
       },
       quertOrderDetail() {
         quertOrderDetail(this.orderId).then(response => {
