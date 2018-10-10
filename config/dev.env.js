cca30c23b5811680f6775ef4247c4c8dbaae1def
cn.shperry <cn.shperry@gmail.com>
2018-10-10 18:07:22 +0800

    rollback cti ws serverurl configuration in dev.

--- config/dev.env.js
+++ config/dev.env.js
@@ -7,5 +7,5 @@ module.exports = merge(prodEnv, {
   // BASE_API: '"http://119.27.179.175:12004/portal"',
   BASE_API: '"/api/v1"',
   TUI_WS_SERVERURL:'"ws://119.27.179.175:12010"',
-  CTI_WS_SERVERURL:'"ws://119.27.184.157:9050/"'
+  CTI_WS_SERVERURL:'"ws://119.27.179.175:9050/"'
 })
