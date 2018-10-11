e710b6f93df90e9b65e6f55be8f1903c5eafeb49
liuhuihuang <liuhh@wise2c.com>
2018-10-11 16:13:11 +0800

    工作台

--- src/views/ord_workingset/index.vue
+++ src/views/ord_workingset/index.vue
@@ -1,150 +1,134 @@
 <template>
   <div class="app-container">
-    <div >
-      <el-row :gutter="20">
-        <el-col :span="6">
+    <div>
+      <el-row :gutter="5">
+        <el-col :span="8">
           <el-card shadow="hover" style="background-color:#339999;">
-            <div class="circle" style="border:solid #339999 1px;background-color:#fff;">
-              <a @click="changeToDailTask('0')"><font class="line-center" style="color:#339999;">{{rowData.firstCallTotal}}</font></a>
-            </div>
-            <div class="p-item">
-              条
-            </div>
-            <div style="float:left;margin:auto 5%">
-               <div><font style="font-size:large;color:#fff;margin-top:1%">总首拨名单</font></div>
-               <div style="position:absolute;bottom:1%">
-                 <a @click="changeToDailPage('0')">
-                  <img src="../../../static/images/ord_workingset/tel.png" alt="拨打" style="width:20%;height:20%"></img>
-                  <font style="font-size:small;color:#fff;">点击开始拨打</font>
-                 </a>
+            <div slot="header" class="clearfix">
+              <div style="display: inline-block;line-height: 50px;cursor: pointer" @click="changeToDailTask('0')">
+                <font style="font-size:large;color:#fff;padding-right: 5px">首拨数量: </font>
+                <font style="font-size:large;color:rgb(255,255,0);">{{new_first_dial_task_count}}</font>
               </div>
             </div>
+            <div class="text item">
+              <el-row>
+                <el-col :span="8" >
+                  <el-card shadow="hover" style="background-color:#f66">
+                      <div class="circle" style="border:solid #f66 1px;background-color:#fff;">
+                        <a @click="changeToDailTask('1')"><font class="line-center" style="color:#f66;">{{new_appoint_contact_task_count}}</font></a>
+                      </div>
+                    <div style="text-align: center">
+                      <font class="text-align-center" style="font-size: small">预约名单</font>
+                    </div>
+                  </el-card>
+                </el-col>
+                <el-col :span="8">
+                  <el-card shadow="hover" style="background-color:#36a9ce">
+                    <div class="circle" style="border:solid #36a9ce 1px;background-color:#fff;">
+                      <a @click="changeToDailTask('2')"><font class="line-center" style="color:#36a9ce;">{{new_success_contact_task_count}}</font></a>
+                    </div>
+                    <div style="text-align: center">
+                      <font class="text-align-center" style="font-size: small">今日成功量</font>
+                    </div>
+                  </el-card>
+                </el-col>
+                <el-col :span="8">
+                  <el-card shadow="hover" style="background-color:#eb7f36">
+                    <div class="circle"  style="border:solid #eb7f36 1px;background-color:#fff;">
+                      <a @click="changeToDailTask('3')"><font class="line-center" style="color:#eb7f36;">{{new_fail_contact_task_count}}</font></a>
+                    </div>
+                    <div style="text-align: center">
+                      <font class="text-align-center" style="font-size: small">今日失败量</font>
+                    </div>
+                  </el-card>
+                </el-col>
+              </el-row>
+            </div>
           </el-card>
         </el-col>
-        <el-col :span="6" >
-          <el-card shadow="hover" style="background-color:#f66"> 
-            <div class="circle" style="border:solid #f66 1px;background-color:#fff;">
-              <a @click="changeToDailTask('1')"><font class="line-center" style="color:#f66;">{{rowData.appointCallTotal}}</font></a>
-            </div>
-            <div class="p-item">
-              条
-            </div>
-            <div style="float:left;margin:auto 5%">
-               <div><font style="font-size:large;color:#fff;margin-top:1%">总预约名单</font></div>
-               <div style="position:absolute;bottom:1%">
-                 <a @click="changeToDailPage('1')">
-                  <img src="../../../static/images/ord_workingset/tel.png" alt="拨打" style="width:20%;height:20%"></img>
-                  <font style="font-size:small;color:#fff;">点击开始拨打</font>
-                 </a>
+        <el-col :span="8">
+          <el-card shadow="hover" style="background-color:#339999;">
+            <div slot="header" class="clearfix">
+              <div style="display: inline-block;line-height: 50px;cursor: pointer" @click="changeToOrderManagement('0')">
+                <font style="font-size:large;color:#fff;padding-right: 5px">订单数量: </font>
+                <font style="font-size:large;color:rgb(255,255,0);">{{count}}</font>
               </div>
             </div>
+            <div class="text item">
+              <el-row>
+                <el-col :span="8" :offset="3">
+                  <el-card shadow="hover" style="background-color:#36a9ce">
+                    <div class="circle" style="border:solid #36a9ce 1px;background-color:#fff;">
+                      <font class="line-center" style="color:#36a9ce;">{{total_amount}}</font>
+                    </div>
+                    <div style="text-align: center">
+                      <font class="text-align-center" style="font-size: small">订单总金额</font>
+                    </div>
+                  </el-card>
+                </el-col>
+                <el-col :span="8" :offset="3">
+                  <el-card shadow="hover" style="background-color:#eb7f36">
+                    <div class="circle"  style="border:solid #eb7f36 1px;background-color:#fff;">
+                      <font class="line-center" style="color:#eb7f36;">{{avg_amount}}</font>
+                    </div>
+                    <div style="text-align: center">
+                      <font class="text-align-center" style="font-size: small">订单平均金额</font>
+                    </div>
+                  </el-card>
+                </el-col>
+              </el-row>
+            </div>
           </el-card>
         </el-col>
-        <el-col :span="6">
-          <el-card shadow="hover" style="background-color:#36a9ce">
-            <div class="circle" style="border:solid #36a9ce 1px;background-color:#fff;">
-              <a @click="changeToDailTask('2')"><font class="line-center" style="color:#36a9ce;">{{rowData.successCallTotal}}</font></a>
+        <el-col :span="8">
+          <el-card shadow="hover" style="background-color:#339999;">
+            <div slot="header" class="clearfix">
+              <div style="display: inline-block;line-height: 50px;">
+                <font style="font-size:medium;color:#fff;padding-right: 5px">通话时间: </font>
+                <font style="font-size:medium;color:rgb(255,255,0);">{{call_time_duration}}</font>
+              </div>
+              <div style="float: right;line-height: 50px">
+                <font style="font-size:medium;color:#fff;padding-right: 5px">通话次数: </font>
+                <font style="font-size:medium;color:rgb(255,255,0);">{{calls_number}}</font>
+              </div>
             </div>
-            <div class="p-item">
-              人次
-            </div>
-            <div style="float:left;margin:auto 5%">
-               <font class="text-align-center">今日成功量</font>
-            </div>
-          </el-card>
-        </el-col>
-        <el-col :span="6">
-          <el-card shadow="hover" style="background-color:#eb7f36">
-            <div class="circle"  style="border:solid #eb7f36 1px;background-color:#fff;">
-              <a @click="changeToDailTask('3')"><font class="line-center" style="color:#eb7f36;">{{rowData.failedCallTotal}}</font></a>
-            </div>
-            <div class="p-item">
-              人次
-            </div>
-            <div style="float:left;margin:auto 5%">
-               <font class="text-align-center">今日失败量</font>
+            <div class="text item">
+              <el-row>
+                <el-col :span="8">
+                  <el-card shadow="hover" style="background-color:#eb7f36">
+                    <div class="circle"  style="border:solid #eb7f36 1px;background-color:#fff;">
+                      <font class="line-center" style="color:#eb7f36;">{{online_time_duration}}</font>
+                    </div>
+                    <div style="text-align: center">
+                      <font class="text-align-center" style="font-size: small">在线时长</font>
+                    </div>
+                  </el-card>
+                </el-col>
+                <el-col :span="8">
+                  <el-card shadow="hover" style="background-color:#36a9ce">
+                    <div class="circle" style="border:solid #36a9ce 1px;background-color:#fff;">
+                      <font class="line-center" style="color:#36a9ce;">{{free_time_duration}}</font>
+                    </div>
+                    <div style="text-align: center">
+                      <font class="text-align-center" style="font-size: small">空闲时长</font>
+                    </div>
+                  </el-card>
+                </el-col>
+                <el-col :span="8">
+                  <el-card shadow="hover" style="background-color:#eb7f36">
+                    <div class="circle"  style="border:solid #eb7f36 1px;background-color:#fff;">
+                      <font class="line-center" style="color:#eb7f36;">{{busy_time_duration}}</font>
+                    </div>
+                    <div style="text-align: center">
+                      <font class="text-align-center" style="font-size: small">示忙时长</font>
+                    </div>
+                  </el-card>
+                </el-col>
+              </el-row>
             </div>
           </el-card>
         </el-col>
       </el-row>
-      <br>
-      <br>
-      <br>
-      <el-row>
-        <div style="border:solid #36a9ce 1px;position:relative">
-          <div style="position:absolute;top:10px;left:10px;" class="squash-item">
-            话务总结
-          </div>
-          <div style="text-align:center">
-            <el-button type="primary" plain @click="findCallInfo('yesterday')">昨天</el-button>
-            <el-button type="primary" plain :autofocus="ischecked" @click="findCallInfo('today')">今天</el-button>
-            <el-button type="primary" plain @click="findCallInfo('month')">整月</el-button>
-          </div>
-          <br>
-          <el-row :gutter="20">
-            <el-col :span="6">
-              <el-card shadow="hover" style="background-color:#339999;">
-                <div class="circle" style="border:solid #339999 1px;background-color:#fff;">
-                  <!-- <a @click="changeToDailInfo()"> -->
-                    <font class="line-center" style="color:#339999;">{{rowInfo.totalCallOut}}</font>
-                    <!-- </a> -->
-                </div>
-                <div class="p-item">
-                  次
-                </div>
-                <div style="float:left;margin:auto 5%">
-                  <div><font class="text-align-center">总拨打量</font></div>
-                </div>
-              </el-card>
-            </el-col>
-
-            <el-col :span="6">
-              <el-card shadow="hover" style="background-color:#339999;">
-                <div class="circle" style="border:solid #339999 1px;background-color:#fff;">
-                  <!-- <a @click="changeToDailInfo()"> -->
-                    <font class="line-center" style="font-size:16px;color:#339999;">{{rowInfo.totalCallTime}}</font>
-                    <!-- </a> -->
-                </div>
-                <div class="p-item" style="visibility:hidden">
-                  时长
-                </div>
-                <div style="float:left;margin:auto 5%">
-                  <div><font class="text-align-center">通话时长</font></div>
-                </div>
-              </el-card>
-            </el-col>
-
-            <el-col :span="6">
-              <el-card shadow="hover" style="background-color:#339999;">
-                <div class="circle" style="border:solid #339999 1px;background-color:#fff;">
-                  <a @click="changeToDailInfo('1')"><font class="line-center" style="color:#339999;">{{rowInfo.answerTotal}}</font></a>
-                </div>
-                <div class="p-item">
-                  次
-                </div>
-                <div style="float:left;margin:auto 5%">
-                  <div><font class="text-align-center">接通量</font></div>
-                </div>
-              </el-card>
-            </el-col>
-
-            <el-col :span="6">
-              <el-card shadow="hover" style="background-color:#eb7f36">
-                  <div class="circle"  style="border:solid #eb7f36 1px;background-color:#fff;">
-                    <a @click="changeToDailInfo('0')"><font class="line-center" style="color:#eb7f36;">{{rowInfo.failedCallTotal}}</font></a>
-                  </div>
-                  <div class="p-item">
-                    次
-                  </div>
-                  <div style="float:left;margin:auto 5%">
-                    <font class="text-align-center">未接通量</font>
-                  </div>
-                </el-card>
-            </el-col>
-          </el-row>
-        </div>
-      </el-row>
-      
     </div>
 
 
@@ -155,12 +139,26 @@
 <script>
 import { Message, MessageBox } from 'element-ui'
 import { formatDateTime } from '@/utils/tools'
+import { getDepartId, obreportAgent, orderreportAgent, reportAgent } from '@/api/ctiReport'
 import { findContactHistory, findTotalContactByTime } from '@/api/ord_workingset'
+import moment from 'moment'
 
 export default {
   name: 'ord_workingset',
   data() {
     return {
+      busy_time_duration: '',
+      call_time_duration: '',
+      calls_number: '',
+      free_time_duration: '',
+      online_time_duration: '',
+      count: '',
+      total_amount: '',
+      avg_amount: '',
+      new_appoint_contact_task_count: '',
+      new_fail_contact_task_count: '',
+      new_first_dial_task_count: '',
+      new_success_contact_task_count: '',
       selectStatus: 'today',
       ischecked: true,
       rowData: {
@@ -174,10 +172,53 @@ export default {
         totalCallTime: '',
         answerTotal: 0,
         failedCallTotal: 0
-      }
+      },
+      departId: ''
     }
   },
   mounted() {
+    getDepartId().then(res => {
+      this.departId = res.data.departId
+      obreportAgent({
+        statistics_type: 'agent',
+        depart_id: this.departId,
+        time_dimension: 'day',
+        agent_id: localStorage.getItem('agentId'),
+        start_time: this.getStartTimestamp(Date.parse(new Date((new Date()).toLocaleDateString()))),
+        end_time: this.getEndTimestamp(Date.parse(new Date((new Date()).toLocaleDateString())))
+      }).then(response => {
+        this.new_appoint_contact_task_count = response.data.result[0].new_appoint_contact_task_count
+        this.new_fail_contact_task_count = response.data.result[0].new_fail_contact_task_count
+        this.new_first_dial_task_count = response.data.result[0].new_first_dial_task_count
+        this.new_success_contact_task_count = response.data.result[0].new_success_contact_task_count
+      })
+      orderreportAgent({
+        statistics_type: 'agent',
+        depart_id: this.departId,
+        time_dimension: 'day',
+        agent_id: localStorage.getItem('agentId'),
+        start_time: this.getStartTimestamp(Date.parse(new Date((new Date()).toLocaleDateString()))),
+        end_time: this.getEndTimestamp(Date.parse(new Date((new Date()).toLocaleDateString())))
+      }).then(response => {
+        this.avg_amount = response.data.result[0].avg_amount
+        this.total_amount = response.data.result[0].total_amount
+        this.count = response.data.result[0].count
+      })
+      reportAgent({
+        statistics_type: 'agent',
+        depart_id: this.departId,
+        time_dimension: 'day',
+        agent_id: localStorage.getItem('agentId'),
+        start_time: this.getStartTimestamp(Date.parse(new Date((new Date()).toLocaleDateString()))),
+        end_time: this.getEndTimestamp(Date.parse(new Date((new Date()).toLocaleDateString())))
+      }).then(response => {
+        this.busy_time_duration = response.data.result[0].busy_time_duration
+        this.call_time_duration = response.data.result[0].call_time_duration
+        this.calls_number = response.data.result[0].calls_number
+        this.free_time_duration = response.data.result[0].free_time_duration
+        this.online_time_duration = response.data.result[0].online_time_duration
+      })
+    })
     findContactHistory().then(res => {
       if (res.data.code === 0) {
         this.rowData.firstCallTotal = res.data.data.noContactCount
@@ -197,6 +238,12 @@ export default {
     })
   },
   methods: {
+    getStartTimestamp(timeStr) {
+      return moment(timeStr, 'x').valueOf()
+    },
+    getEndTimestamp(timeStr) {
+      return moment(timeStr, 'x').add(1, 'days').subtract(1, 'ms').valueOf()
+    },
     findCallInfo(obj) {
       findTotalContactByTime(obj).then(res => {
         if (res.data.code === 0) {
@@ -261,6 +308,13 @@ export default {
       })
       sessionStorage.removeItem('isDialTask')
       sessionStorage.removeItem('quickDialto')
+    },
+    changeToOrderManagement() {
+      this.$router.push({
+        name: 'order_management.html',
+        query: { 'startTime': formatDateTime(this.getStartTimestamp(Date.parse(new Date((new Date()).toLocaleDateString())))),
+          'endTime': formatDateTime(this.getEndTimestamp(Date.parse(new Date((new Date()).toLocaleDateString())))) }
+      })
     }
   }
 }
@@ -270,15 +324,16 @@ export default {
   padding: 2%
 }
 .circle{
-  width:80px;
-  height:80px;
+  width:6em;
+  height:40px;
   border-radius:40px;
-  float:left;
+  /*float:left;*/
+  margin: 0 auto;
   text-align:center;
 }
 .line-center{
-  line-height:80px;
-  font-size:24px;
+  line-height:40px;
+  font-size:14px;
   font-weight:bold;
 }
 .p-item{
@@ -290,7 +345,7 @@ export default {
   font-size:large;
   color:#fff;
   margin-top:1%;
-  line-height:80px;
+  line-height:50px;
 }
 .squash-item{
   width: 80px;
