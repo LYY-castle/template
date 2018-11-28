d4748c3cac10a1bec232eac0b3a1e7ee97bb4e5c
cn.shperry <cn.shperry@gmail.com>
2018-11-28 18:30:50 +0800

    optimize no operate time warn logic.

--- src/components/Charts/orderChart.vue
+++ src/components/Charts/orderChart.vue
@@ -1530,26 +1530,22 @@
           pageSize: 10
         }
 
-        if (this.timeValueClone[0] && this.timeValueClone[0].getDate()) {
-          params.start_time = this.getStartTimestamp(Date.parse(this.timeValueClone[0]), this.formInline.timeClone)
-        } else {
+        if (!this.timeValueClone[0] || !this.timeValueClone[1]) {
           Message({
-            message: '请选择开始时间',
+            message: '请选择操作时间',
             type: 'error',
             duration: 3 * 1000
           })
           return
+        }
+
+        if (this.timeValueClone[0] && this.timeValueClone[0].getDate()) {
+          params.start_time = this.getStartTimestamp(Date.parse(this.timeValueClone[0]), this.formInline.timeClone)
         }
         if (this.timeValueClone[1] && this.timeValueClone[1].getDate()) {
           params.end_time = this.getEndTimestamp(Date.parse(this.timeValueClone[1]), this.formInline.timeClone)
-        } else {
-          Message({
-            message: '请选择结束时间',
-            type: 'error',
-            duration: 3 * 1000
-          })
-          return
         }
+
         if (this.statistics_type === 'depart') {
           params.sub_depart_id = this.formInline.sub_depart_id.join(',')
         } else {
