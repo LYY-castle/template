470b7f63e55f7d42373e20c2ec2b71e3c00a7f8d
liusl@wise2c.com <liusl@wise2c.com>
2018-12-14 16:00:38 +0800

    修改tab栏无法滚动问题

--- src/components/ScrollPane/index.vue
+++ src/components/ScrollPane/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="scroll-container" ref="scrollContainer" @wheel.prevent="handleScroll">
-    <div class="scroll-wrapper" ref="scrollWrapper" :style="{left: left + 'px'}">
+    <div class="scroll-wrapper" ref="scrollWrapper" :style="{left: left +'px'}">
       <slot></slot>
     </div>
   </div>
@@ -23,9 +23,11 @@ export default {
       const $containerWidth = $container.offsetWidth
       const $wrapper = this.$refs.scrollWrapper
       const $wrapperWidth = $wrapper.offsetWidth
-
+      console.log('eventDelta', eventDelta)
+      console.log(this.left)
       if (eventDelta > 0) {
         this.left = Math.min(0, this.left + eventDelta)
+        console.log('left', this.left)
       } else {
         if ($containerWidth - padding < $wrapperWidth) {
           if (this.left < -($wrapperWidth - $containerWidth + padding)) {
@@ -41,9 +43,8 @@ export default {
     moveToTarget($target) {
       const $container = this.$refs.scrollContainer
       const $containerWidth = $container.offsetWidth
-      const $targetLeft = $target.offsetLeft
-      const $targetWidth = $target.offsetWidth
-
+      const $targetLeft = $target.$el.offsetLeft
+      const $targetWidth = $target.$el.offsetWidth
       if ($targetLeft < -this.left) {
         // tag in the left
         this.left = -$targetLeft + padding
