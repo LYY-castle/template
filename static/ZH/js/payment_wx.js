23d29c9a92ed9ab8b2709c4d6ab390ea8d5a8270
liusl@wise2c.com <liusl@wise2c.com>
2019-02-11 16:06:11 +0800

    修改微信支付演示页面金额

--- static/ZH/js/payment_wx.js
+++ static/ZH/js/payment_wx.js
@@ -3,10 +3,6 @@ var theme = 'auto';
 // Init App
 var app = new Framework7();
 
-/*展示空的订单信息*/
-$("#orderId").html(" 201901310000001");
-$("#totalCount").html("1.00元").css("color","red");
-
 $(".gopay").on('click',function(){
   console.log("111")
   manualAlert("正在跳转到微信支付，请稍后...");
