a687ea3782730d4d92e60ded681baef895047faf
LYY <liyy@wise2c.com>
2019-04-18 17:23:41 +0800

    拉取master更新

--- src/router/dynamic-router.js
+++ src/router/dynamic-router.js
@@ -11,21 +11,29 @@ export default (menuData) => {
   }
 
   const getRouterForMenu = (menu, parentMenu) => {
-    menu.path = menu.value ? `/${menu.value}` : ''
+    menu.level = S(menu.idPath).count('/') - 1
+    menu.path = menu.value ? (menu.level > 1 ? `${menu.value}` : `/${menu.value}`) : ''
     menu.component = parentMenu ? (menu.value ? () => import('@/views/' + menu.value + '/index') : null) : Layout
     menu.hidden = menu.status === '0'
     menu.meta = {
       title: menu.name,
       icon: menu.icon
     }
-    menu.level = S(menu.idPath).count('/') - 1
     menu.alwaysShow = menu.value ? true : (menu.children && menu.children.length)
+    if (menu.level === 1 && menu.value) {
+      menu.redirect = `${menu.value}/index`
+    }
   }
   const fillRecursiveRoutersFromMenus = (menus, parentMenu) => {
     menus.forEach(menu => {
       getRouterForMenu(menu, parentMenu)
       if (menu.children && menu.children.length) {
         fillRecursiveRoutersFromMenus(menu.children, menu)
+      } else if (!parentMenu) {
+        menu.children = [{
+          path: 'index',
+          component: menu.value ? () => import('@/views/' + menu.value + '/index') : null
+        }]
       }
     })
   }
